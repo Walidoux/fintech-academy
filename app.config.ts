9efacd372a777a5c4792de713a7b0253c701162e
Walidoux <ma.walidkorchi@icloud.com>
2026-01-06 16:14:56 +0100

    test: clean up wisely

--- app.config.ts
+++ app.config.ts
@@ -1,33 +1,33 @@
-import { defineConfig } from "@solidjs/start/config";
-import tsconfigPaths from "vite-tsconfig-paths";
-import contentCollections from "@content-collections/solid-start";
+import contentCollections from '@content-collections/solid-start'
+import { defineConfig } from '@solidjs/start/config'
 import tailwindcss from '@tailwindcss/vite'
+import pkg from '@vinxi/plugin-mdx'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-/* @ts-ignore */
-import pkg from "@vinxi/plugin-mdx";
-const { default: mdx } = pkg;
-import remarkFrontmatter from "remark-frontmatter";
+const { default: mdx } = pkg
+
+import remarkFrontmatter from 'remark-frontmatter'
 
 export default defineConfig({
-	extensions: ["mdx"],
-	vite: {
-		plugins: [
-		  tailwindcss(),
-			tsconfigPaths(),
-			contentCollections(),
-			mdx.withImports({})({
-				jsx: true,
-				jsxImportSource: "solid-js",
-				providerImportSource: "~/tools/solid-mdx",
-				remarkPlugins: [remarkFrontmatter],
-			}),
-		],
-	},
-	server: {
-		prerender: {
-			crawlLinks: true,
-		},
-		// see https://github.com/solidjs/solid-start/issues/1614
-		esbuild: { options: { target: "esnext" } },
-	},
-});
+  extensions: ['mdx'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+      tsconfigPaths(),
+      contentCollections(),
+      mdx.withImports({})({
+        jsx: true,
+        jsxImportSource: 'solid-js',
+        providerImportSource: '~/tools/solid-mdx',
+        remarkPlugins: [remarkFrontmatter],
+      }),
+    ],
+  },
+  server: {
+    prerender: {
+      crawlLinks: true,
+    },
+    // see https://github.com/solidjs/solid-start/issues/1614
+    esbuild: { options: { target: 'esnext' } },
+  },
+})
--- src/tools/solid-mdx.tsx
+++ src/tools/solid-mdx.tsx
@@ -1,24 +1,23 @@
 // Acts like a JSX factory for compiling MDX documents.
 
-import { A } from "@solidjs/router";
-import type { Component } from "solid-js";
+import { A } from '@solidjs/router'
+import type { Component } from 'solid-js'
 
-
-import { Typography as BaseTypography } from "~/components/base-typography";
-import { Typography as CustomTypography } from "~/components/custom-typography";
+import { Typography as BaseTypography } from '~/components/base-typography'
+import { Typography as CustomTypography } from '~/components/custom-typography'
 
 // biome-ignore lint/suspicious/noExplicitAny: different components have different props
 export const useMDXComponents: () => Record<string, Component<any>> = () => ({
   ...BaseTypography,
   ...CustomTypography,
 
-	a: (props) => <A {...props} />,
-	hr: (props) => <hr {...props} />,
-	i: (props) => <i {...props} />,
-	b: (props) => <b {...props} />,
-	em: (props) => <em {...props} />,
-	strong: (props) => <strong {...props} />,
-	li: (props) => <li {...props} />,
-	ol: (props) => <ol {...props} />,
-	pre: (props) => <pre {...props} />,
-});
+  a: (props) => <A {...props} />,
+  hr: (props) => <hr {...props} />,
+  i: (props) => <i {...props} />,
+  b: (props) => <b {...props} />,
+  em: (props) => <em {...props} />,
+  strong: (props) => <strong {...props} />,
+  li: (props) => <li {...props} />,
+  ol: (props) => <ol {...props} />,
+  pre: (props) => <pre {...props} />,
+})
