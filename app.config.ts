dba2df558298a5f7ff5b8f33b5df2abd3eb12527
Walidoux <ma.walidkorchi@icloud.com>
2026-01-06 21:43:17 +0100

    that was not it

--- app.config.ts
+++ app.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
         jsxImportSource: 'solid-js',
         providerImportSource: '~/tools/solid-mdx',
         remarkPlugins: [remarkFrontmatter, remarkMath],
-        rehypePlugins: [[rehypekatex, { throwOnError: false, output: 'html' }]],
+        rehypePlugins: [rehypekatex],
       }),
     ],
   },
